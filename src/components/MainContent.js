5926ccaf7efb5aab5323a11a1e16180b30ff68c7
Stewart Alsop <stewartalsop@192.168.1.27>
2024-01-09 18:47:57 -0300

    first commit

---
+++ src/components/MainContent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainContent() {
+  return (
+    <main>
+      <p>This is the main section of the landing page.</p>
+    </main>
+  );
+}
+
+export default MainContent;
